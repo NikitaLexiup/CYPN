92076f6f79dc1a0021bb3701750885cac06cb7e5
Nikita Ivanov <106706979+NikitaLexiup@users.noreply.github.com>
2023-07-04 03:05:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.querySelector('.btnConvert').addEventListener('click', convert);
 function convert() {
 
     let inputNumber = document.querySelector('.input_text').value;
-    let convertNumber = inputNumber.replace(/[-\(\)\_\.\,\ ]/g, '');
+    let convertNumber = inputNumber.replace(/[^+\d]/g, '');
     document.querySelector('.out').innerHTML = convertNumber;  
     
-    
+
     document.querySelector('.btnCopy').addEventListener('click', copyResult);
 
     function copyResult() {
